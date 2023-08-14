daada900fd459c93f3f93c0a5b5b0c53ca8ad1d6
Hyun Seo <hyundotio@gmail.com>
2023-08-13 21:05:53 -0400

    Fixed bug where unselecting a category or orbit while tracking an effected object will not reset camera

--- components/satellites/CesiumView/index.jsx
+++ components/satellites/CesiumView/index.jsx
@@ -94,7 +94,6 @@ const CesiumView = ({ recentLaunches, setLoadingStatus }) => {
     //Handle unselecting selected objects if it is not visible anymore
     for (let i = 0; i < selectedEntitiesLen; i++) {
       const selectedEntity = selectedEntities[i];
-
       const validObjectCategory = objectCategories.some(c => c.visible && c.name.toLowerCase() === selectedEntity.categoryName.toLowerCase());
       const validOrbitCategory = orbitCategories.some(c => c.visible && c.type === selectedEntity.orbitType);
 
@@ -110,6 +109,11 @@ const CesiumView = ({ recentLaunches, setLoadingStatus }) => {
           LabelStyle: helperFunctionsRef.current.LabelStyle,
           VerticalOrigin: helperFunctionsRef.current.VerticalOrigin,
         });
+
+        if (isTracking === selectedEntity.id) {
+          //On top of unselecting, reset camera if visible item is tracked.
+          resetCamera();
+        }
       }
     }
     setSelectedEntities(newSelectedEntities);
